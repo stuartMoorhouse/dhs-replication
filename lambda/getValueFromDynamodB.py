0cb2646db7abb5f59b80da28477025f738b3d95e
Stuart Moorhouse <Stuart.Moorhouse@marklogic.com>
2020-06-03 16:05:22 +0200

    fixed scoping error in lambda/getValueFromDynamoDB

--- lambda/getValueFromDynamodB.py
+++ lambda/getValueFromDynamodB.py
@@ -2,28 +2,30 @@ import json
 import os
 import boto3
 
-table = event["table"]
-keyname = event["keyname"]
-key = event["key"]
+
 
 # Environment variables that can be used to debug:
 # table = os.environ['table']
 # keyname = os.environ['keyname']
 # key = os.environ['key']
 
-client = boto3.client('dynamodb')
-dynamo_DB_response = client.get_item(TableName=table, Key={keyname: { 'S': key } }) 
-timestamp = dynamo_DB_response["Item"]["timestamp"]["S"]
-
-json_output = {
-  "timestamp": timestamp,
-  "collection": key
-}
 
 
 def lambda_handler(event, context):
 
+    table = event["table"]
+    keyname = event["keyname"]
+    key = event["key"]
+
+    client = boto3.client('dynamodb')
+    dynamo_DB_response = client.get_item(TableName=table, Key={keyname: { 'S': key } }) 
+    timestamp = dynamo_DB_response["Item"]["timestamp"]["S"]
+
+    json_output = {
+     "timestamp": timestamp,
+     "collection": key
+    }
+    
     return {
         'body': json_output
     }
-
